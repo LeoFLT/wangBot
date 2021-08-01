35d01bb3df0941966d077ec9526a0e48ce4ebec2
LeoFLT <dev@leoflt.com>
2021-08-01 11:04:39 -0300

    refactor functions.js

--- functions.js
+++ functions.js
@@ -1,15 +1,3 @@
-const ReactionListener = require('./models/Reaction');
-
-async function detectMemberFromReaction(user, reaction) {
-  console.log(
-    `${new Date().toISOString().replace('T', ' ')} - INFO:\t`
-    + `${user.tag} reacted with :${reaction.emoji}: on message "${reaction.message.id}"`,
-  );
-  return reaction.message.guild.members.cache.find(
-    (m) => m.user.id === user.id,
-  );
-}
-
 module.exports = {
   parseCmd(cmd, msg, noBackticks) {
     if (noBackticks) {
@@ -29,100 +17,32 @@ module.exports = {
     if (!strNoFilter.startsWith('`') || !strNoFilter.endsWith('`')) return;
     return strNoFilter.replace(/`/g, '').trim();
   },
-  async userAddRole(client, discordObj) {
-    const { user, reaction } = discordObj;
-    const member = await detectMemberFromReaction(user, reaction);
-    if (!member) return;
-
-    try {
-      process.stdout.write(
-        `${new Date()
-          .toISOString()
-          .replace('T', ' ')} - INFO:\tFetching message from DB...`,
-      );
-      const messageQuery = await ReactionListener.findById(reaction.message.id);
-
-      process.stdout.write(messageQuery ? ' Found\n' : ' Not Found\n');
-      if (!messageQuery?.is_ongoing) return;
-      const roleToApply = messageQuery.availableRoles.get(reaction.emoji.name);
-      const roleList = [];
-      const removeArr = [];
-
-      if (messageQuery.allow_dupes) {
-        await member.roles.add(roleToApply, `added via reaction to a poll created by ${messageQuery.message.author.tag}`
-          + ` on channel #${messageQuery.channel.name}`);
-      } else {
-        messageQuery.availableRoles.forEach((v) => roleList.push(v.id));
-        member.roles.cache.forEach((role) => {
-          if (roleList.includes(role.id)) removeArr.push(role);
-        });
-        await removeArr.forEach((r) => member.roles.remove(r, `removed via reaction to a poll created by ${messageQuery.message.author.tag}`
-          + ` on channel #${messageQuery.channel.name}`));
-
-        await member.roles.add(roleToApply.id, `added via reaction to a poll created by ${messageQuery.message.author.tag}`
-          + ` on channel #${messageQuery.channel.name}`);
-        messageQuery.users.set(user.id, roleToApply.id);
-        await messageQuery.save().then(console.log);
-      }
-    } catch (e) {
-      console.log(`${new Date().toISOString()
-        .replace('T', ' ')} - ERROR:\t${e.message}\nStack: ${e.stack}`);
+  discordTimestamp(dateObj, format) {
+    let dateFormat;
+    switch (format) {
+      case 'isoDate':
+        dateFormat = 'd';
+        break;
+      case 'date':
+        dateFormat = 'D';
+        break;
+      case 'dateTime':
+        dateFormat = 'f';
+        break;
+      case 'weekDayDateTime':
+        dateFormat = 'F';
+        break;
+      case 'time':
+        dateFormat = 't';
+        break;
+      case 'timeLong':
+        dateFormat = 'T';
+        break;
+      case 'timeElapsed':
+      default:
+        dateFormat = 'R';
+        break;
     }
-  },
-  async userRemoveRole(client, discordObj) {
-    const { user, reaction } = discordObj;
-    const member = await detectMemberFromReaction(user, reaction);
-    if (!member) return;
-
-    try {
-      process.stdout.write(
-        `${new Date()
-          .toISOString()
-          .replace('T', ' ')} - INFO:\tFetching message from DB...`,
-      );
-      const messageQuery = await ReactionListener.findById(reaction.message.id);
-      process.stdout.write(messageQuery ? ' Found\n' : ' Not Found\n');
-      if (!messageQuery) return;
-
-      const roleToRemove = messageQuery.availableRoles.get(reaction.emoji.name);
-      if (roleToRemove) {
-        await member.roles.remove(roleToRemove.id,
-          `removed via reaction to a poll created by ${messageQuery.message.author.tag}`
-          + ` on channel #${messageQuery.channel.name}`);
-        messageQuery.users.set(user.id, undefined);
-        await messageQuery.save();
-      }
-    } catch (e) {
-      console.log(`${new Date().toISOString()
-        .replace('T', ' ')} - ERROR:\t${e.message}\nStack: ${e.stack}`);
-    }
-  },
-  async fetchReactionList(dClient) {
-    let allMessages = [];
-    try {
-      process.stdout.write(
-        `${new Date()
-          .toISOString()
-          .replace('T', ' ')} - INFO:\tFetching messages from DB...`,
-      );
-      allMessages = await ReactionListener.find({
-        is_ongoing: true,
-      });
-      process.stdout.write(' OK!');
-    } catch (e) {
-      process.stdout.write(`\nError: ${e.message}\nStack: ${e.stack}\n`);
-    }
-    let messageCount = 0;
-    await Promise.all(allMessages.map(async (msg) => {
-      const channel = await dClient.guilds.cache
-        .get(msg.guild)
-        .channels.cache.get(msg.channel.id);
-      channel.messages.fetch(msg.id).then(() => messageCount++).catch(console.log);
-      messageCount++;
-    }));
-    process.stdout.write(` (${messageCount} messages cached)\n`);
-  },
-  discordTimestamp(dateObj) {
-    return `<t:${Math.floor(dateObj ? dateObj.getTime() / 1000 : Date.now() / 1000)}:R>`;
+    return `<t:${Math.floor(dateObj ? dateObj.getTime() / 1000 : Date.now() / 1000)}:${dateFormat}>`;
   },
 };
